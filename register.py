c77bfc8aa72d4ac37c77120f653a052ae395e727
Seb1903 <sebastien.martinez99@gmail.com>
2020-03-31 00:40:31 +0200

    Complete working registration

--- register.py
+++ register.py
@@ -1,8 +1,13 @@
-
-
-def sendJSON(socket, data):
-    	msg = json.dumps(data).encode('utf8')
-	total = 0
-	while total < len(msg):
-		sent = socket.send(msg[total:])
-		total += sent
+import json 
+import socket 
+def register(variableport) : 
+    host = socket.gethostname()
+    port = variableport
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+    with open('inscription.json') as file: 
+        msg = file.read().encode('utf8')
+        total = 0
+        while total < len(msg):
+            sent = s.send(msg[total:])
+            total += sent
