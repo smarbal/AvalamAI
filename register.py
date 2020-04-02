b0a27775ecaf1368982d6dc6d2e525791b5535ba
Seb1903 <sebastien.martinez99@gmail.com>
2020-04-02 10:58:36 +0200

    Launch 2 instances correctly

--- register.py
+++ register.py
@@ -9,17 +9,6 @@ if int(sys.argv[1]) < 5000 :
         d = json.dumps(data)
     with open('inscription.json','w') as file :    
         file.write(d)
-    def register(variableport) : 
-        host = socket.gethostname()
-        port = variableport
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host,port))
-        with open('inscription.json') as file: 
-            msg = file.read().encode('utf8')
-            total = 0
-            while total < len(msg):
-                sent = s.send(msg[total:])
-                total += sent
 
 if int(sys.argv[1]) > 5000 : 
     with open('inscriptionbis.json','r') as file : 
@@ -28,14 +17,25 @@ if int(sys.argv[1]) > 5000 :
         d = json.dumps(data)
     with open('inscriptionbis.json','w') as file :    
         file.write(d)
-    def register(variableport) : 
-        host = socket.gethostname()
-        port = variableport
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host,port))
+
+    
+def register(variableport) : 
+    host = socket.gethostname()
+    port = variableport
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+    if int(sys.argv[1]) < 5000 : 
+        with open('inscription.json') as file: 
+            msg = file.read().encode('utf8')
+            total = 0
+            while total < len(msg):
+                sent = s.send(msg[total:])
+                total += sent
+    if int(sys.argv[1]) > 5000 : 
         with open('inscriptionbis.json') as file: 
             msg = file.read().encode('utf8')
             total = 0
             while total < len(msg):
                 sent = s.send(msg[total:])
                 total += sent
+    
