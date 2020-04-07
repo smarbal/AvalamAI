5f4989685d8e634024c221a67caf51880e0ca410
Seb1903 <sebastien.martinez99@gmail.com>
2020-04-08 00:23:52 +0200

    Correct negative value for list

--- main.py
+++ main.py
@@ -43,13 +43,15 @@ class Avalam(TwoPlayersGame):
                 if tour != [] : 
                     for c in range(-1,1) : 
                         for d in range(-1,1) : 
-                            othertower = self.board[a+c][b+d] 
-                            if othertower != [] :
-                                if c == d == 0 : 
-                                    pass
-                                elif len(tower) + len(othertower) <= 5 : 
-                                    return othertower.extend(tower)
-                                    return tower.clear()    #va falloir tout inclure en un return, donc trouver une manière d'expliciter un mouvement d'une tour à l'autre (faire comme le prof p-e ? sauf que string ), ou alors une liste comme dans le quick example 
+                            if a+c > 0 : 
+                                if b+d > 0 : 
+                                    othertower = self.board[a+c][b+d] 
+                                    if othertower != [] :
+                                        if c == d == 0 : 
+                                            pass
+                                        elif len(tower) + len(othertower) <= 5 : 
+                                            return othertower.extend(tower)
+                                            return tower.clear()    #va falloir tout inclure en un return, donc trouver une manière d'expliciter un mouvement d'une tour à l'autre (faire comme le prof p-e ? sauf que string ), ou alors une liste comme dans le quick example 
  # possiblement créer une liste vide dans possible, à la fin de la fonction faire append dans cette liste des coord, faire un return de toute la liste à la toute fin, en dehors des boucles                                    
     def make_move(self,move) : 
         othertower.extend(tower)
