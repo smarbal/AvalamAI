d2a21cabc63c780fae5d41d081a494ee2f7fe52a
Seb1903 <sebastien.martinez99@gmail.com>
2020-04-08 13:37:33 +0200

    add win and ending control

--- main.py
+++ main.py
@@ -34,14 +34,15 @@ class Avalam(TwoPlayersGame):
     def __init__(self, players):
         self.players = players
         self.nplayer = 1    #si joueur 1 commence, à àméliorer si joueur 2 commence, à voir avec le fichier json 
-        self.board = body['game'] 
+        self.board = body['game']
+     
     
     def possible_moves(self):
         moves = []
         for a in range(9): 
             for b in range(9) :
                 tower = self.board[a][b]
-                if tour != [] : 
+                if tower != [] : 
                     for c in range(-1,2) : 
                         for d in range(-1,2) : 
                             if a+c >= 0 and a+c < 9: 
@@ -58,6 +59,26 @@ class Avalam(TwoPlayersGame):
     def make_move(self,move) : 
         self.board[move[1][0]][move[1][1]].extend(self.board[[move[0][0]][[move[0][1]]) #on verse ce qui se trouve dans la premiere tour dans la deuxieme
         self.board[self.board[[move[0][0]][[move[0][1]]].clear() #on vide la première tour 
+
+    def win(self) :
+        playerpiece = body['players'].index('Ton IA va faire Aie')
+        playerlist = []   # mettre self ? 
+        opponentlist = []
+        if is_over() is True : 
+            for a in range(9): 
+                for b in range(9) :
+                    tower = self.board[a][b]
+                    if tower != [] :
+                        piece = tower.pop()
+                        if piece = playerpiece : 
+                            playerlist.append(piece)
+                        else : 
+                            opponentlist.append(piece)
+        return len(playerlist) > len(opponentlist)
+            
+
+    def is_over(self) : 
+        return self.possible_moves == []     
 
 
 
