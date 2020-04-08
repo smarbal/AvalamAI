1cd55174218052e43a31bb0785f8a28c3d3feefe
Seb1903 <sebastien.martinez99@gmail.com>
2020-04-09 00:00:47 +0200

    Complete fully fuctionning AI

--- main.py
+++ main.py
@@ -19,23 +19,101 @@ class Server:
         cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
         if cherrypy.request.method == "OPTIONS":
             return ''
-        
+
         body = cherrypy.request.json
-        
-        if len(body['moves']) < 6  :
+
+        class Avalam(TwoPlayersGame):
+            def __init__(self, players):
+                self.players = players
+                self.nplayer = 1    #si joueur 1 commence, à àméliorer si joueur 2 commence, à voir avec le fichier json 
+                self.board = body['game']
+                self.playerwon = False
+                self.thegameisover = False
+
+
+            def possible_moves(self):
+                self.moves = []
+                for a in range(9): 
+                    for b in range(9) :
+                        tower = self.board[a][b]
+                        if tower != [] : 
+                            for c in range(-1,2) : 
+                                for d in range(-1,2) : 
+                                    if a+c >= 0 and a+c < 9: 
+                                        if b+d >= 0 and b+d <9: 
+                                            othertower = self.board[a+c][b+d] 
+                                            if othertower != [] :
+                                                if c == d == 0 : 
+                                                    pass
+                                                elif len(tower) + len(othertower) <= 5 : 
+                                                    self.moves.append([[a,b],[a+c,b+d], len(tower)])  
+                                                        # on ajoute aux moves possibles les coordonées de respectivement la première et deuxième tour. exemple de move : [[0, 3], [0, 4]]
+                return self.moves
+                print(self.moves)
+
+
+
+            def make_move(self,move) : 
+                self.board[move[1][0]][move[1][1]].extend(self.board[move[0][0]][move[0][1]]) #on verse ce qui se trouve dans la premiere tour dans la deuxieme
+                self.board[move[0][0]][move[0][1]].clear() #on vide la première tour
+
+            def unmake_move(self, move):
+                i = 0
+                oldtower = []
+                while i < move[2] :
+                    lastpiece = self.board[move[1][0]][move[1][1]].pop()
+                    self.board[move[0][0]][move[0][1]].append(lastpiece)
+                    i += 1
+                
+
+
+            def win(self):
+                playerpiece = body['players'].index('TonIA va faire Aie')
+                opponentlist = []  
+                playerlist = []
+                if self.thegameisover() is True :  
+                    for a in range(9): 
+                        for b in range(9) :
+                            tower = self.board[a][b]
+                            if tower != [] :
+                                piece = tower.pop()
+                                if piece == playerpiece : 
+                                    playerlist.append(piece)
+                                else : 
+                                    opponentlist.append(piece)
+                return len(playerlist) > len(opponentlist)
+                self.playerwon = len(playerlist) > len(opponentlist)
+
+            def is_over(self) : 
+                return self.possible_moves() == []
+                self.thegameisover = self.possible_moves() == []
+                    
+            
+            def scoring(self):
+                    if self.playerwon is True : 
+                        return 100 
+                    else : 
+                        return 0 
+                
+            def show(self) :
+                print(self.board) 
+                
+     
+        if len(body['moves']) <= 25  :
             ai_algo = Negamax(3)
             ai_algo2 = Negamax(3)
         
-        if len(body['moves']) > 6  :
-            ai_algo = Negamax(8)
-            ai_algo2 = Negamax(8)
+        if len(body['moves']) > 25  :
+            ai_algo = Negamax(4)
+            ai_algo2 = Negamax(4)
         game =  Avalam([AI_Player(ai_algo), AI_Player(ai_algo2)])
-        game.play(1) 
-        movelist = [[5,6],[5,7]]
+        game.play(1)
+        movelist = game.get_move()
+
         return {"move": {
             
-        'from' : [5,6],
-        'to':    [5,7]
+        'from' : movelist[0],
+        'to':    movelist[1]
         
         },
         "message" : "Bien le bonjour"
@@ -44,83 +122,11 @@ class Server:
     @cherrypy.expose
     def ping(self):
         return "pong"
-
-
-class Avalam(TwoPlayersGame):
-    def __init__(self, players):
-        self.players = players
-        self.nplayer = 1    #si joueur 1 commence, à àméliorer si joueur 2 commence, à voir avec le fichier json 
-        self.board = body['game']
-        self.playerwon = False
-        self.thegameisover = False
-
-
-    def possible_moves(self):
-        self.moves = []
-        for a in range(9): 
-            for b in range(9) :
-                tower = self.board[a][b]
-                if tower != [] : 
-                    for c in range(-1,2) : 
-                        for d in range(-1,2) : 
-                            if a+c >= 0 and a+c < 9: 
-                                if b+d >= 0 and b+d <9: 
-                                    othertower = self.board[a+c][b+d] 
-                                    if othertower != [] :
-                                        if c == d == 0 : 
-                                            pass
-                                        elif len(tower) + len(othertower) <= 5 : 
-                                            self.moves.append([[a,b],[a+c,b+d], len(tower)])  
-                                                # on ajoute aux moves possibles les coordonées de respectivement la première et deuxième tour. exemple de move : [[0, 3], [0, 4]]
-        return self.moves
-        print(self.moves)
-
-
-
-    def make_move(self,move) : 
-        self.board[move[1][0]][move[1][1]].extend(self.board[move[0][0]][move[0][1]]) #on verse ce qui se trouve dans la premiere tour dans la deuxieme
-        self.board[move[0][0]][move[0][1]].clear() #on vide la première tour
-
-    def unmake_move(self, move):
-        i = 0
-        oldtower = []
-        while i < move[2] :
-            lastpiece = self.board[move[1][0]][move[1][1]].pop()
-            self.board[move[0][0]][move[0][1]].append(lastpiece)
-            i += 1
-        
-
-
-    def win(self):
-        playerpiece = body['players'].index('LUR')
-        opponentlist = []  
-        playerlist = []
-        if self.thegameisover() is True :  
-            for a in range(9): 
-                for b in range(9) :
-                    tower = self.board[a][b]
-                    if tower != [] :
-                        piece = tower.pop()
-                        if piece == playerpiece : 
-                            playerlist.append(piece)
-                        else : 
-                            opponentlist.append(piece)
-        return len(playerlist) > len(opponentlist)
-        self.playerwon = len(playerlist) > len(opponentlist)
-
-    def is_over(self) : 
-        return self.possible_moves() == []
-        self.thegameisover = self.possible_moves() == []
-            
     
-    def scoring(self):
-            if self.playerwon is True : 
-                return 100 
-            else : 
-                return 0 
-        
-    def show(self) :
-        print(self.board) 
+
+
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
