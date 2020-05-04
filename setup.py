b23c65085e958525460f13433c3dc33f71ceba11
Seb1903 <sebastien.martinez99@gmail.com>
2020-05-04 14:58:43 +0200

    putted all setup in one file

--- setup.py
+++ setup.py
@@ -5,3 +5,11 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize("c_is_alone.pyx"),
 )
+
+setup(
+    ext_modules=cythonize("c_possible_moves.pyx"),
+)
+
+setup(
+    ext_modules=cythonize("c_win_tower.pyx"),
+)
--- setup2.py
+++ setup2.py
@@ -5,3 +5,7 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize("c_possible_moves.pyx"),
 )
+
+setup(
+    ext_modules=cythonize("c_win_tower.pyx"),
+)
